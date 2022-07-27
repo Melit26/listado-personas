05bdb7a25d7328a338b85cbaef6bbef9d1afdc26
Melit26 <yuligonzalez26@gmail.com>
2022-07-27 14:51:52 +0200

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   titulo = 'Listado de Personas';
+
+  constructor(){}
+
+    ngOnInit(): void {
+      firebase.initializeApp({
+        apiKey: "AIzaSyCS8aaE8_PD9CTgUBhmT-Jqit09syKPvO0",
+        authDomain: "listado-personas-c3fc7.firebaseapp.com",
+      })
+    }
 
 }
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import firebase from 'firebase/compat/app';
+import { Router } from '@angular/router';
+import 'firebase/compat/auth';
+
+@Injectable()
+export class LoginService {
+
+  token: string;
+
+  constructor(private router: Router){}
+
+  login(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password).
+      then(
+        response => {
+          firebase.auth().currentUser?.getIdToken().then(
+            token => {
+              this.token = token;
+            }
+          )
+        }
+      )
+      this.router.navigate(['/']);
+  }
+
+  getIdToken(){
+    return this.token;
+  }
+}
